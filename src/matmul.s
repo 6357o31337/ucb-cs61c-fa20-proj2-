f1175d709b39f2095bb280076c8d69be145075f0
6357o31337 <68189875+6357o31337@users.noreply.github.com>
2020-12-30 08:16:37 +0900

    change

--- src/matmul.s
+++ src/matmul.s
@@ -1,3 +1,5 @@
+# .import dot.s
+
 .globl matmul
 
 .text
@@ -24,40 +26,80 @@
 #     this function terminates the program with exit code 74.
 # =======================================================
 matmul:
-
+    ebreak
     # Error checks
 
 
     # Prologue
+    addi sp, sp, -8
+    sw s0, 0(sp)
+    sw ra, 4(sp)
+    
+    
+    
+    mv t0 zero
+    mv t1, a1
+    mv a1, a3
+    # mv t3, a4
+    mv t6, a6
+    # mv t7, a3
 
+
+    # li a3 4
+    li a3 1 #
+
+    # mul a4, a3, a5 # a4 := 4 * (# of col of m1)
+    mv a4, a5
+
+    # mul t4, a3, a2 # t4 := 4 * (# of col of m0)
+    li s0, 4
+    mul t4, s0, a2
+
+    mul t3, s0, a4
 
 outer_loop_start:
+    # we are done once t0 >= t1
+    bge t0, t1, outer_loop_end
+
+    mv t5, zero
 
 
-
+    # mv a1, t7
 
 inner_loop_start:
+    # inner_loop end once t5 >= a2
+    bge t5, a5, inner_loop_end
+
+
+    jal ra dot
+    sw a0, 0(t6)
+
+    add t6, t6, s0 # 4
+    add a1, a1, s0
 
 
 
-
-
-
-
-
+    addi t5, t5, 1
+    j inner_loop_start
 
 
 
 
 inner_loop_end:
 
+    sub a1, a1, t3 # a1 := a1 - 4*(# of col of m1)
 
+    add a0, a0, t4 # t3 := t3 + 4*(# of col of m0)
 
+    addi t0, t0, 1
+    j outer_loop_start
 
 outer_loop_end:
 
 
     # Epilogue
-    
+    lw s0, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
     
     ret
